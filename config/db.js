743b7f21c4e1f2a0896a59ecad4a7f68deba7d04
Hussein <husseinsym3@gmail.com>
2024-05-30 00:54:39 +0300

    fix pg issue

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectModule: require("pg"),
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
